d8cbfacf7dfe6e96526f2ffc5d8cb5d866a2b61c
Axel Dahlin <axdahlin@gmail.com>
2022-01-25 14:59:31 +0100

    add Bold.cs

---
+++ MarkdownParser/Bold.cs
@@ -0,0 +1,29 @@
+namespace dotnet_visitor;
+
+public class Bold : IHtmlTag
+{
+    public List<IHtmlTag> Children { get; set; }
+
+    public string TextContent { get; set; }
+    public string TextToParse { get; set; }
+
+    public Bold(string textToParse)
+    {
+        TextToParse = textToParse;
+        Children = new List<IHtmlTag>();
+    }
+    public string Accept(AbstractVisitor visitor)
+    {
+        return visitor.VisitBold(this);
+
+    }
+    public void AddChild(IHtmlTag child){
+        this.Children.Add(child);
+    }
+    public void SetTextContent(string textContent){
+        this.TextContent = textContent;
+    }
+    public void SetTextToParse(string textToParse) {
+        this.TextToParse = textToParse;
+    }
+}
