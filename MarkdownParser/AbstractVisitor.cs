dcfd3455244b736a6a395612e75fd91ed40e6d4d
Axel Dahlin <axdahlin@gmail.com>
2022-01-25 15:02:07 +0100

    add AbstractVisitor.cs

---
+++ MarkdownParser/AbstractVisitor.cs
@@ -0,0 +1,11 @@
+using System.Text.RegularExpressions;
+namespace dotnet_visitor
+{
+    public abstract class AbstractVisitor
+    {
+        public abstract string VisitParagraph(Paragraph paragraph);
+        public abstract string VisitBold(Bold bold);
+        public abstract string VisitItalic(Italic italic);
+        public abstract string VisitRoot(Root root);
+    }
+}
