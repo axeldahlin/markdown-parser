161d94469721eaafb9216d9818c57863266e905e
Axel Dahlin <axdahlin@gmail.com>
2022-01-25 14:58:55 +0100

    add IHtmlTag.cs

---
+++ MarkdownParser/IHtmlTag.cs
@@ -0,0 +1,13 @@
+namespace dotnet_visitor
+{
+    public interface IHtmlTag
+    {
+        public List<IHtmlTag> Children { get; set; }
+        public string TextToParse { get; set; }
+        public string TextContent { get; set; }
+        public void AddChild(IHtmlTag child);
+        public void SetTextContent(string textContent);
+        public void SetTextToParse(string textToParse);
+        public string Accept(AbstractVisitor visitor);
+    }
+}
